768bec65d1fe59b604be6040b4777a765029cdd2
Frankie96 <frankie_96@windowslive.com>
2020-10-13 11:36:00 +0200

    diverses

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php require_once('templates/header.php'); ?> 
+<?php require_once('templates/header.php'); ?>
 
 <div class="container">
     <h1>Hello, world!</h1>
-
 </div>
 
-<?php require_once('templates/footer.php'); ?>
+<div class="container">
+    <?php require_once('templates/footer.php'); ?>
+</div>
--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,25 @@
 <!doctype html>
 <html lang="de">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/style/style.css">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Hello, world!</title>
+  <meta name="description" content="Description">
+  <meta name="keywords" content="SDG, SDG12, Sustainability">
+  <meta name="author" content="Sandro Anderes, Frank Zinsli">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+  <!-- CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/style/style.css">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+
+</head>
+
+<body>

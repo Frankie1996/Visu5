a5d748e169b35650dbb97dad53ac81c52ba96d30
Sandro Anderes <sandroanderes@outlook.com>
2020-10-16 19:08:44 +0200

    finished slider accordion

--- templates/header.php
+++ templates/header.php
@@ -1,28 +1,28 @@
 <div class="accordion">
-  <ul>
-    <li>
-      <div> <a href="#">
-        <h1>Nachhaltigkeit</h1>
-        <p>Description 1</p>
-        </a> </div>
-    </li>
-    <li>
-      <div> <a href="#">
-        <h1>Klimaveränderung</h1>
-        <p>Description 2</p>
-        </a> </div>
-    </li>
-    <li>
-      <div> <a href="#">
-        <h1>Nachhaltige Energie</h1>
-        <p>Description 3</p>
-        </a> </div>
-    </li>
-    <li>
-      <div> <a href="#">
-        <h1>Entsorgung & Recylcing</h1>
-        <p>Description 3</p>
-        </a> </div>
-    </li>
-  </ul>
+    <ul>
+        <li>
+            <div> <a href="#">
+                    <h1>Nachhaltigkeit</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
+                </a> </div>
+        </li>
+        <li>
+            <div> <a href="#">
+                    <h1>Klimaveränderung</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
+                </a> </div>
+        </li>
+        <li>
+            <div> <a href="#">
+                    <h1>Nachhaltige Energie</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
+                </a> </div>
+        </li>
+        <li>
+            <div> <a href="#">
+                    <h1>Entsorgung & Recylcing</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
+                </a> </div>
+        </li>
+    </ul>
 </div>

92973f2af8c1324f5eafe5f95bb1a9c1713687d5
Sandro Anderes <sandroanderes@outlook.com>
2020-10-16 18:33:22 +0200

    added new h1 font

--- templates/header.php
+++ templates/header.php
@@ -2,25 +2,25 @@
   <ul>
     <li>
       <div> <a href="#">
-        <h2>Title 1</h2>
+        <h1>Nachhaltigkeit</h1>
         <p>Description 1</p>
         </a> </div>
     </li>
     <li>
       <div> <a href="#">
-        <h2>Title 2</h2>
+        <h1>Klimaveränderung</h1>
         <p>Description 2</p>
         </a> </div>
     </li>
     <li>
       <div> <a href="#">
-        <h2>Title 3</h2>
+        <h1>Nachhaltige Energie</h1>
         <p>Description 3</p>
         </a> </div>
     </li>
     <li>
       <div> <a href="#">
-        <h2>Title 3</h2>
+        <h1>Entsorgung & Recylcing</h1>
         <p>Description 3</p>
         </a> </div>
     </li>

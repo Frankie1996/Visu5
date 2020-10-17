9f3f1d840395c422b8eeb78fbe08e929b0b4eb07
Frankie96 <frankie_96@windowslive.com>
2020-10-17 13:39:32 +0200

    lessons db queries

--- data/data.php
+++ data/data.php
@@ -1,30 +1,32 @@
-<?php 
+<?php
 require_once('env.php');
 
-function get_db_connection(){
+function get_db_connection()
+{
 
-global $db_host, $db_name, $db_user, $db_pass, $db_charset;
-$dsn = "mysql:host=$db_host;dbname=$db_name;charset=$db_charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false
-];
+	global $db_host, $db_name, $db_user, $db_pass, $db_charset;
+	$dsn = "mysql:host=$db_host;dbname=$db_name;charset=$db_charset";
+	$options = [
+		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_EMULATE_PREPARES   => false
+	];
 
-try {
-    $db = new PDO($dsn, $db_user, $db_pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+	try {
+		$db = new PDO($dsn, $db_user, $db_pass, $options);
+	} catch (\PDOException $e) {
+		throw new \PDOException($e->getMessage(), (int)$e->getCode());
+	}
 
-return $db;
+	return $db;
 }
 
 
 /******************** SELECT QUERIES *********************/
 
 /* GET ALL INTROS */
-function get_all_intros(){
+function get_all_intros()
+{
 	$db = get_db_connection();
 	$sql = "SELECT * FROM tbl_intro;";
 	$result = $db->query($sql);
@@ -32,7 +34,8 @@ function get_all_intros(){
 }
 
 /* GET ALL LESSONS */
-function get_all_lessons(){
+function get_all_lessons()
+{
 	$db = get_db_connection();
 	$sql = "SELECT * FROM tbl_lesson;";
 	$result = $db->query($sql);
@@ -40,7 +43,8 @@ function get_all_lessons(){
 }
 
 /* GET TOP LESSONS */
-function get_top_lessons(){
+function get_top_lessons()
+{
 	$db = get_db_connection();
 	$sql = "SELECT * FROM tbl_lesson WHERE top_three = 1;";
 	$result = $db->query($sql);
@@ -48,16 +52,18 @@ function get_top_lessons(){
 }
 
 /* GET CATEGORY BY ID */
-function get_category_by_id($id){
+function get_category_by_id($id)
+{
 	$db = get_db_connection();
 	$sql = "SELECT * FROM tbl_category WHERE id = $id;";
 	$result = $db->query($sql);
-    $row = $result->fetch();
-    return $row;
+	$row = $result->fetch();
+	return $row;
 }
 
 /* GET LESSONS BY CATEGORY_ID */
-function get_lessons_by_category_id($id){
+function get_lessons_by_category_id($id)
+{
 	$db = get_db_connection();
 	$sql = "SELECT * FROM tbl_lesson WHERE category_id = $id;";
 	$result = $db->query($sql);
@@ -65,7 +71,8 @@ function get_lessons_by_category_id($id){
 }
 
 /* GET ABOUT */
-function get_about(){
+function get_about()
+{
 	$db = get_db_connection();
 	$sql = "SELECT * FROM tbl_about";
 	$result = $db->query($sql);
@@ -74,10 +81,11 @@ function get_about(){
 }
 
 /* GET LESSON BY ID */
-function get_lesson_by_id($id){
+function get_lesson_by_id($id)
+{
 	$db = get_db_connection();
 	$sql = "SELECT * FROM tbl_lesson WHERE id = $id;";
 	$result = $db->query($sql);
-    $row = $result->fetch();
-    return $row;
+	$row = $result->fetch();
+	return $row;
 }
--- index.php
+++ index.php
@@ -10,11 +10,11 @@ $topLessons = get_top_lessons();
 <?php require_once('templates/head.php'); ?>
 
 <nav>
-<?php require_once('templates/navbar.php'); ?>
+    <?php require_once('templates/navbar.php'); ?>
 </nav>
 
 <header>
-<?php require_once('templates/header.php'); ?>
+    <?php require_once('templates/header.php'); ?>
 </header>
 
 <section class="container">
@@ -25,42 +25,35 @@ $topLessons = get_top_lessons();
     <h5>Hello, world!</h5>
     <h6>Hello, world!</h6>
     <p>Hello, world!</p>
+
     <article>
-        <h2>Alle Themenbereiche aufgelistet:</h2>
-        <?php foreach($allIntros as $intro) { ?>
-            <ul>
-                <li><?php echo $intro['title'] ?></li>
-            </ul>
-        <?php } ?>
-    </article>
-    <article>
-    <h2>Top Lektionen</h2>
+        <h2>Top Lektionen</h2>
         <div class="card-group">
-        <?php foreach($topLessons as $lesson) { ?>
-            <a href="lesson.php?id=<?php echo $lesson['id']; ?>">
-            <div class="card col-md-4 col-sm-12">
-                <img class="card-img-top" src="<?php echo $lesson['gif_url'] ?>">
-                <div class="card-body">
-                    <h3 class="card-title"><?php echo $lesson['title'] ?></h3>
-                    <p class="card-text">
-                    <small class="text-muted">
-                            <a href="category.php?id=<?php echo $lesson['category_id']; ?>">#
-                                <?php 
-                                $category =  get_category_by_id($lesson['category_id']);
-                                echo $category['title']; 
-                                echo " · "; 
-                                ?>
-                            </a>
-                            <?php 
-                                echo "Lektion "; 
-                                echo $lesson['lesson_nr'];
-                            ?>
-                        </small>
-                    </p>
-                </div>
-            </div>
-            </a>
-        <?php } ?>
+            <?php foreach ($topLessons as $lesson) { ?>
+                <a href="lesson.php?id=<?php echo $lesson['id']; ?>">
+                    <div class="card col-md-4 col-sm-12">
+                        <img class="card-img-top" src="<?php echo $lesson['gif_url'] ?>">
+                        <div class="card-body">
+                            <h3 class="card-title"><?php echo $lesson['title'] ?></h3>
+                            <p class="card-text">
+                                <small class="text-muted">
+                                    <a href="category.php?id=<?php echo $lesson['category_id']; ?>">#
+                                        <?php
+                                        $category =  get_category_by_id($lesson['category_id']);
+                                        echo $category['title'];
+                                        echo " · ";
+                                        ?>
+                                    </a>
+                                    <?php
+                                    echo "Lektion ";
+                                    echo $lesson['lesson_nr'];
+                                    ?>
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            <?php } ?>
         </div>
     </article>
 </section>

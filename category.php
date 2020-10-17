b91db0addfb339e0f6b61f1cc517c9b40f5814d3
Frankie96 <frankie_96@windowslive.com>
2020-10-17 12:34:33 +0200

    clean the code

--- category.php
+++ category.php
@@ -1,68 +1,25 @@
 <!-- Controller -->
 <?php
-require_once('../data/config.php');
-require_once('../data/data.php');
+require_once('data/config.php');
+require_once('data/data.php');
 
 $allIntros = get_all_intros();
 $topLessons = get_top_lessons();
 ?>
 
 <!-- Head -->
-<?php require_once('../templates/head.php'); ?>
+<?php require_once('templates/head.php'); ?>
 
 <nav>
-<?php require_once('../templates/navbar.php'); ?>
+<?php require_once('templates/navbar.php'); ?>
 </nav>
 
-<header>
-<?php require_once('../templates/header.php'); ?>
-</header>
-
 <section class="container">
-    <h1>Hello, world!</h1>
-    <h2>Hello, world!</h2>
-    <h3>Hello, world!</h3>
-    <h4>Hello, world!</h4>
-    <h5>Hello, world!</h5>
-    <h6>Hello, world!</h6>
-    <p>Hello, world!</p>
+    <h1>Einzelne Kategorie</h1>
     <article>
-        <h2>Alle Themenbereiche aufgelistet:</h2>
-        <?php foreach($allIntros as $intro) { ?>
-            <ul>
-                <li><?php echo $intro['title'] ?></li>
-            </ul>
-        <?php } ?>
-    </article>
-    <article>
-    <h2>Top Lektionen</h2>
-        <div class="card-group">
-        <?php foreach($topLessons as $lesson) { ?>
-            <div class="card col-md-4 col-sm-12">
-                <img class="card-img-top" src="<?php echo $lesson['gif_url'] ?>">
-                <div class="card-body">
-                    <h3 class="card-title"><?php echo $lesson['title'] ?></h3>
-                    <p class="card-text">
-                        <small class="text-muted">
-                        <a href="#">#
-                                <?php 
-                                $category =  get_category_by_id($lesson['category_id']);
-                                echo $category['title']; 
-                                echo " · "; 
-                                ?>
-                            </a>
-                            <?php 
-                                echo "Lektion "; 
-                                echo $lesson['lesson_nr'];
-                            ?>
-                        </small>
-                    </p>
-                </div>
-            </div>
-        <?php } ?>
-        </div>
+
     </article>
 </section>
 
 <!-- Footer -->
-<?php require_once('../templates/footer.php'); ?>
+<?php require_once('templates/footer.php'); ?>
---
+++ lesson.php
@@ -0,0 +1,25 @@
+<!-- Controller -->
+<?php
+require_once('data/config.php');
+require_once('data/data.php');
+
+$allIntros = get_all_intros();
+$topLessons = get_top_lessons();
+?>
+
+<!-- Head -->
+<?php require_once('templates/head.php'); ?>
+
+<nav>
+<?php require_once('templates/navbar.php'); ?>
+</nav>
+
+<section class="container">
+    <h1>Einzelne Lektion</h1>
+    <article>
+
+    </article>
+</section>
+
+<!-- Footer -->
+<?php require_once('templates/footer.php'); ?>

8eb02a2670814f7d09f007fa7b0fd850fde84166
Frankie96 <frankie_96@windowslive.com>
2020-10-17 15:15:57 +0200

    Update lessons.php

--- lessons.php
+++ lessons.php
@@ -9,7 +9,7 @@ $allLessons = get_all_lessons();
 <?php require_once('templates/head.php'); ?>
 
 <nav>
-    <?php require_once('templates/navbar.php'); ?>
+<?php require_once('templates/navbar.php'); ?>
 </nav>
 
 
@@ -18,31 +18,31 @@ $allLessons = get_all_lessons();
     <article>
         <h2>Lektionen</h2>
         <div class="card-group">
-            <?php foreach ($allLessons as $lesson) { ?>
-                <a href="lesson.php?id=<?php echo $lesson['id']; ?>">
-                    <div class="card col-md-4">
-                        <img src="<?php echo $lesson['gif_url'] ?>" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title"> <?php echo $lesson['title'] ?> </h5>
-                            <p class="card-text">
-                                <small class="text-muted">
-                                    <a href="category.php?id=<?php echo $lesson['category_id']; ?>">#
-                                        <?php
-                                        $category =  get_category_by_id($lesson['category_id']);
-                                        echo $category['title'];
-                                        echo " · ";
-                                        ?>
-                                    </a>
-                                    <?php
-                                    echo "Lektion ";
-                                    echo $lesson['lesson_nr'];
-                                    ?>
-                                </small>
-                            </p>
-                        </div>
-                    </div>
-                </a>
-            <?php } ?>
+        <?php foreach($allLessons as $lesson) { ?>
+            <a href="lesson.php?id=<?php echo $lesson['id']; ?>">
+            <div class="card col-md-4">
+                <img src="<?php echo $lesson['gif_url'] ?>" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title"> <?php echo $lesson['title'] ?> </h5>
+                    <p class="card-text">
+                        <small class="text-muted">
+                            <a href="category.php?id=<?php echo $lesson['category_id']; ?>">#
+                                <?php 
+                                $category =  get_category_by_id($lesson['category_id']);
+                                echo $category['title']; 
+                                echo " · "; 
+                                ?>
+                            </a>
+                            <?php 
+                                echo "Lektion "; 
+                                echo $lesson['lesson_nr'];
+                            ?>
+                        </small>
+                    </p>
+                </div>
+            </div>
+            </a>
+        <?php } ?>
         </div>
     </article>
 </section>

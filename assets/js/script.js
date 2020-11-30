d5b786b94a39791e16ee22cc101f7eb243060a45
Frankie96 <frankie_96@windowslive.com>
2020-11-30 14:23:20 +0100

    active class + Gif

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ window.onload = activeClass(window.location.href.split('/').pop());
 function activeClass(url) {
     if (url == "") {
         document.querySelector("#nav-home").classList.add("active");
-    } else if(url == "about.php"){
+    } else if(url.startsWith('about') == true){
         document.querySelector("#nav-about").classList.add("active");
-    } else if(url == "lessons.php") {
+    } else if(url.startsWith('lesson') == true) {
         document.querySelector("#nav-lessons").classList.add("active");
     }
 }

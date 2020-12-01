69b752243d185635d2c922e32bec84e00e6fccf9
Sandro Anderes <sandroanderes@outlook.com>
2020-12-01 16:22:31 +0100

    fixed grammer errors and links

--- lesson.php
+++ lesson.php
@@ -67,7 +67,7 @@ $biggest_problem = explode(", ", $biggest_problems);
                     <?php } ?>
 
                 <?php } else { ?>
-                    <h3 class="mb-4">Das wichtigste in Kürze</h3>
+                    <h3 class="mb-4">Das Wichtigste in Kürze</h3>
                     <ul>
                         <?php for ($i = 0; $i < count($important) - 1; ++$i) { ?>
                             <li><?php echo $important[$i] ?></li>

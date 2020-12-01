f90134ba36bd8a9525c9ae0ce8fbd6c4182a2e41
Frankie96 <frankie_96@windowslive.com>
2020-12-01 14:00:30 +0100

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,25 +1,25 @@
 <div class="accordion">
     <ul>
         <li>
-            <div> <a href="#">
+            <div> <a href="./category.php?id=2">
                     <h1>Nachhaltigkeit</h1>
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
                 </a> </div>
         </li>
         <li>
-            <div> <a href="#">
+            <div> <a href="./">
                     <h1>Klimaveränderung</h1>
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
                 </a> </div>
         </li>
         <li>
-            <div> <a href="#">
+            <div> <a href="./category.php?id=1">
                     <h1>Nachhaltige Energie</h1>
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
                 </a> </div>
         </li>
         <li>
-            <div> <a href="#">
+            <div> <a href="./">
                     <h1>Entsorgung & Recylcing</h1>
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.</p>
                 </a> </div>

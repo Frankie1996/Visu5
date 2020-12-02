022a4a3023643a95c4866c3b3f1f541484ea3d77
Frankie96 <frankie_96@windowslive.com>
2020-12-02 16:22:41 +0100

    favicons

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,13 +1,14 @@
-<nav class="navbar navbar-icon-top navbar-expand navbar-light bg-light <?php echo isset($category['title']) ? strtolower($category['title']) : ''; ?>">
+<nav class="navbar navbar-icon-top navbar-expand navbar-light bg-light  <?php echo isset($category['title']) ? strtolower($category['title']) : ''; ?>">
     <div class="container">
+
+
         <a class="navbar-brand" href=".">
             <img src="assets/logo/logo.png" alt="ökoLOGISCH" loading="lazy">
         </a>
 
+
         <div class="navbar-expand">
-            <ul class="navbar-nav mr-auto">
-            </ul>
-            <ul class="navbar-nav">
+            <ul class="navbar-nav ">
                 <li class="nav-item">
                     <a class="nav-link" id="nav-home" href=".">
                         <span class="material-icons">
@@ -41,4 +42,5 @@
             </ul>
         </div>
     </div>
+
 </nav>

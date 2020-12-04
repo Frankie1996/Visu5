354feb666a2a98555fe05ca6d3a48c7f6db741fe
Frankie96 <frankie_96@windowslive.com>
2020-12-04 11:28:04 +0100

    memory link

--- index.php
+++ index.php
@@ -79,33 +79,28 @@ $topLessons = get_top_lessons();
 <section class="container-fluid experience">
     <div class="container">
         <h3 class="mb-2">Wissenstest</h3>
-        <p class="mb-4">Von hier aus kannst du direkt auf unsere Quizrunden und Spiele zugreifen.</p>
+        <p class="mb-4">Teste dein erarbeitetes Wissen mit einem Minigame.</p>
         <div class="row">
-            <?php
-            foreach ($topLessons as $lesson) {
-                $category =  get_category_by_id($lesson['category_id']);
-            ?>
+
                 <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-                    <a class="link-title" href="lesson?id=<?php echo $lesson['id']; ?>">
-                        <div class="card <?php echo strtolower($category['title']); ?>">
-                            <img src="<?php echo $lesson['gif_url_static'] ?>" class="card-img-top gif_static">
-                            <img src="<?php echo $lesson['gif_url'] ?>" class="card-img-top gif">
+                    <a class="link-title" href="memory">
+                        <div class="card energie">
+                            <img src="assets/gif/memory.jpg" class="card-img-top gif_static">
+                            <img src="assets/gif/memory.gif" class="card-img-top gif">
                             <div class="card-body">
-                                <h5 class="card-title text-center"><?php echo $lesson['title'] ?></h5>
+                                <h5 class="card-title text-center">Memory</h5>
                                 <p class="card-text small text-center">
-                                    <b><a href="category?id=<?php echo $lesson['category_id']; ?>">
-                                            <?php echo "#" . $category['title']; ?></b>
+                                    <b><a href="category?id=1">
+                                            # Energie</b>
                     </a>
                     •
                     <?php
-                    echo "Lektion ";
-                    echo $lesson['lesson_nr'];
+                    echo "Minigame";
                     ?></p>
                 </div>
         </div>
         </a>
     </div>
-<?php } ?>
 </div>
 </div>
 </section>
